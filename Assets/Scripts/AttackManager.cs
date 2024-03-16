567bf53180362c1dc348eb4020bde2db87a57928
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-03-17 01:58:22 +0900

    feat: add firing and dry firing sound

--- Assets/Scripts/AttackManager.cs
+++ Assets/Scripts/AttackManager.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackManager : MonoBehaviour
 {
     public GameObject bullet, bulletPool, player; // bulletPool을 구별!
+    public AudioClip fireSound, dryFireSound;
     public float bulletSpeed = 40;
     public int bulletCount = 6;
 
 
     public int bulletIndex = 0;
     public List<GameObject> bullets;
+    public AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
         bullets = new List<GameObject>();
+        audio = GetComponent<AudioSource>();
 
         for (int i = 0; i < bulletCount; ++i)
         {
@@ -38,17 +42,28 @@ public class AttackManager : MonoBehaviour
     {
         if (bulletIndex < bullets.Count) // List.Count; List의 길이 
         {
-            bullets[bulletIndex].transform.position = this.transform.position;
-            bullets[bulletIndex].SetActive(true);
-            bullets[bulletIndex].GetComponent<Rigidbody>().velocity = Vector3.right * bulletSpeed * (player.GetComponent<PlayerManager>().isfront ? 1 : -1);//transform.parent.transform.localScale.x;
+            fire(bullets[bulletIndex]);
             ++bulletIndex;
         }
-        else {
+        else
+        {
             noBulletEffect();
         }
+        audio.Play();
     }
 
-    void noBulletEffect() { // https://pixabay.com/sound-effects/search/revolver/?pagi=2&duration=0-30
+    void fire(GameObject bullet)
+    {
+        audio.clip = fireSound;
+        bullet.transform.position = this.transform.position;
+        bullet.GetComponent<Rigidbody>().velocity = Vector3.right * bulletSpeed * (player.GetComponent<PlayerManager>().isfront ? 1 : -1);
+        bullet.SetActive(true);
+        //transform.parent.transform.localScale.x;
+    }
+
+    void noBulletEffect()
+    { // https://pixabay.com/sound-effects/search/revolver/?pagi=2&duration=0-30
+        audio.clip = dryFireSound;
 
     }
 }
